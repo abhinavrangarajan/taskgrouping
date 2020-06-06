2a310ebe3512c0f993cc2fdbd85f7306f6032bb4
naagnes <naagnes@stanford.edu>
2020-06-06 20:41:38 +0000

    Creates N and 5N training splits

---
+++ create_train_splits.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import random
+import copy
+
+N = int(sys.argv[1])
+
+task_folder = os.path.join(os.getcwd(), "../taskonomy_full_dataset/rgb/")
+
+names = []
+for building in os.listdir(task_folder):
+    if os.path.isdir(task_folder + building):
+        names.append([building, len(os.listdir(task_folder + building))])
+
+names.sort(key=lambda x: x[1])
+print("Buildings:", names)
+
+# Create N dataset
+tot = 0
+num = 0
+num_buildings = len(names)
+train_dataset = set()
+while tot < N and num < num_buildings:
+    possible_buildings = []
+    for i in range(num_buildings):    
+        if names[i][0] not in train_dataset and tot + names[i][1] < N:
+            possible_buildings.append(i)
+    if len(possible_buildings) == 0:
+        break
+    new = random.choice(possible_buildings)
+    train_dataset.add(names[new][0])
+    tot += names[new][1]
+    num += 1
+
+tot0 = tot
+print("Created N training dataset with {} images (from {} buildings).".format(tot, num))
+
+# Create now 5N dataset
+train5_dataset = copy.copy(train_dataset)
+
+while tot < 5*N and num < num_buildings:
+    possible_buildings = []
+    for i in range(num_buildings):    
+        if names[i][0] not in train5_dataset and tot + names[i][1] < 5*N:
+            possible_buildings.append(i)
+    if len(possible_buildings) == 0:
+        break
+    new = random.choice(possible_buildings)
+    train5_dataset.add(names[new][0])
+    tot += names[new][1]
+    num += 1
+
+print("Created 5N training dataset with {} images (from {} buildings).".format(tot, num))
+
+# Write now these to file
+with open("trainN_models.txt", "w") as f:
+    f.write("\n".join(train_dataset))
+
+with open("train5N_models.txt", "w") as f:
+    f.write("\n".join(train5_dataset))
+
+print("\nN dataset has {} labels. 5N dataset has {} labels.".format(tot0, tot))
